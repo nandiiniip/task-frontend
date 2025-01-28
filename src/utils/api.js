905c1831e370172c91b8311432eb833013b0788f
nandiiniip <nandinip@terawe.com>
2025-01-28 11:30:51 +0530

    interceptor added

---
+++ src/utils/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request interceptor for adding auth token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Response interceptor for handling errors
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      // Handle 401 Unauthorized errors
+      if (error.response.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+      // Handle other errors
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
